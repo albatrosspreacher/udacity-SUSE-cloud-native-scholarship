146b7f7f36f1936d7dabf979c63e7bd35fdc704d
Nandini Proothi <46650119+nandiniproothi@users.noreply.github.com>
2021-06-13 22:49:20 +0530

    :up: added comments and formatted

--- exercises/1-endpoints/app.py
+++ exercises/1-endpoints/app.py
@@ -1,25 +1,26 @@
-from flask import Flask
+from flask import Flask  #import the flask library
 
-app = Flask(__name__)
+app = Flask(
+    __name__)  #creates a flask instance with the name of the current module
 
-@app.route("/")
-def hello():
-    return "Hello World!"
 
-@app.route("/status")
-def status():
-    healthz = {
-        "result": "OK - healthy"
-    }
-    return healthz, 200
+@app.route("/")  #adds a route / to the flask application
+def hello():  #defines a function that is called when the app route is /
+    return "Hello World!"  #returns this value
 
-@app.route("/metrics")
-def metrics():
-    metrics = {
-        "UserCount": 140,
-        "UserCountActive": 23
-    }
-    return metrics, 200
+
+@app.route("/status")  #adds a route /status to the flask application
+def status():  #defines a function that is called when the app route is /status
+    healthz = {"result": "OK - healthy"}
+    return healthz, 200  #returns the variable along with status code 200
+
+
+@app.route("/metrics")  #adds a route /metrics to the flask application
+def metrics(
+):  #defines a function that is called when the app route is /metrics
+    metrics = {"UserCount": 140, "UserCountActive": 23}
+    return metrics, 200  #returns the variable along with status code 200
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
--- exercises/2-logging/app.py
+++ exercises/2-logging/app.py
@@ -1,30 +1,35 @@
-from flask import Flask
-import logging
+from flask import Flask  #import the flask library
+import logging  #import logging library
 
-app = Flask(__name__)
-logging.basicConfig(filename = 'app.log', level=logging.DEBUG, format = f'%(asctime)s %(module)s : %(message)s')
+app = Flask(
+    __name__)  #creates a flask instance with the name of the current module
+logging.basicConfig(
+    filename='app.log',
+    level=logging.DEBUG,
+    format=f'%(asctime)s %(module)s : %(message)s'
+)  #set the basic configuration of the logger along with the format for log messages
 
-@app.route("/")
-def hello():
-    app.logger.debug("/ endpoint was reached!")
-    return "Hello World!"
 
-@app.route("/status")
-def status():
-    app.logger.debug("/status endpoint was reached!")
-    healthz = {
-        "result": "OK - healthy"
-    }
-    return healthz, 200
+@app.route("/")  #adds a route / to the flask application
+def hello():  #defines a function that is called when the app route is /
+    app.logger.debug("/ endpoint was reached!")  #logs a message
+    return "Hello World!"  #returns this value
 
-@app.route("/metrics")
-def metrics():
-    app.logger.debug("/metrics endpoint was reached!")
-    metrics = {
-        "UserCount": 140,
-        "UserCountActive": 23
-    }
-    return metrics, 200
+
+@app.route("/status")  #adds a route /status to the flask application
+def status():  #defines a function that is called when the app route is /status
+    app.logger.debug("/status endpoint was reached!")  #logs a message
+    healthz = {"result": "OK - healthy"}
+    return healthz, 200  #returns the variable along with status code 200
+
+
+@app.route("/metrics")  #adds a route /metrics to the flask application
+def metrics(
+):  #defines a function that is called when the app route is /metrics
+    app.logger.debug("/metrics endpoint was reached!")  #logs a message
+    metrics = {"UserCount": 140, "UserCountActive": 23}
+    return metrics, 200  #returns the variable along with status code 200
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
